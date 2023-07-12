5542b58c8e174cd1baac4ed98ed090fcad10275e
SPeak <speakshen@163.com>
2023-07-12 14:28:51 +0800

    config submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
+#include <iostream>
+
 #include "KeyHistory.h"
 
 int main() {
+    
+    // config window
     dsvisual::PlatformManager::setRootWindowName("Key History @ Power by DSVisual");
     dsvisual::PlatformManager::setRootWindowSize(860, 960);
+    
+    // create keyhistory
     khistory::KeyHistory kh;
+
+    // windows closed check
     while (!dsvisual::PlatformManager::windowClosed()) {
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
+
     return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -1,30 +1,34 @@
---includes("third-party/DSViusal/xmake.lua")
+add_rules("mode.debug", "mode.release")
 
-set_languages("c++17")
+set_project("key-history")
 
--- libs
+set_languages("cxx17")
+
+-- config common third-party libs
 add_requires("imgui 1.88", {configs = {glfw_opengl3 = true}})
-
-add_includedirs(
-    "include",
-    "third-party/DSVisual",
-    "third-party/DStruct"
-)
-
 add_packages("imgui")
-
+-- use dsvisual framework
+add_includedirs("third-party/DSVisual")
 add_files("third-party/DSVisual/dsv-core/dsvisual-core.cpp")
-
+add_includedirs("third-party/DStruct")
 add_links("X11")
 
-add_files("platform/linux_platform_impl.cpp")
+-- config os platform
+if is_host("windows") then
+    add_files("platform/win_platform_impl.cpp")
+elseif is_host("macosx") then
+    add_files("platform/mac_platform_impl.cpp")
+else
+    add_files("platform/linux_platform_impl.cpp")
+end
 
-target("KeyHistory-test")
-    set_kind("binary")
-    add_cxxflags("-g")
-    add_files("tests/test.cpp")
+-- config key-history include path
+add_includedirs("include")
 
 target("KeyHistory")
     set_kind("binary")
-    add_cxxflags("-g")
     add_files("src/*.cpp")
+
+target("KeyHistory-test")
+    set_kind("binary")
+    add_files("tests/test.cpp")
