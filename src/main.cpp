59e28cb05e67d39dcc8169ffd7cdbcfae4ebc37e
Sunrisepeak <speakshen@163.com>
2023-08-04 15:36:48 +0800

    add copyright info

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,16 @@
+// Use of this source code is governed by GPL3-License
+// that can be found in the License file.
+//
+// Copyright (C) 2023 - present
+//
+// Author: Sunrisepeak/SPeak/言峰 (speakshen@163.com)
+// ProjectLinks: https://github.com/Sunrisepeak/KHistory
+//
+// Modify History:
+//        2023/8/4   sunrisepeak    add license
+//
+//
+
 #include <dsvisual.hpp>
 
 #include "KeyHistory.h"
